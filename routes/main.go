d064ee1a10613b470211dc0907aeabb39b22cabd
chintansakhiya <123356373+chintansakhiya@users.noreply.github.com>
2023-05-01 11:56:08 +0530

    remove unnecessary files (#3)

--- routes/main.go
+++ routes/main.go
@@ -1,13 +1,11 @@
 package routes
 
 import (
-	"fmt"
 	"sync"
 
 	"go.uber.org/zap"
 
 	"github.com/Improwised/golang-api/config"
-	"github.com/Improwised/golang-api/constants"
 	controller "github.com/Improwised/golang-api/controllers/api/v1"
 	"github.com/Improwised/golang-api/middlewares"
 	"github.com/Improwised/golang-api/pkg/events"
@@ -30,22 +28,7 @@ func Setup(app *fiber.App, goqu *goqu.Database, logger *zap.Logger, config confi
 		return c.Render("./assets/index.html", fiber.Map{})
 	})
 
-	router := app.Group("/api")
-	v1 := router.Group("/v1")
-
-	middlewares := middlewares.NewMiddleware(config, logger)
-
-	err := setupAuthController(v1, goqu, logger, config)
-	if err != nil {
-		return err
-	}
-
-	err = setupUserController(v1, goqu, logger, middlewares, events)
-	if err != nil {
-		return err
-	}
-
-	err = healthCheckController(app, goqu, logger)
+	err := healthCheckController(app, goqu, logger)
 	if err != nil {
 		return err
 	}
@@ -59,26 +42,7 @@ func Setup(app *fiber.App, goqu *goqu.Database, logger *zap.Logger, config confi
 	return nil
 }
 
-func setupAuthController(v1 fiber.Router, goqu *goqu.Database, logger *zap.Logger, config config.AppConfig) error {
-	authController, err := controller.NewAuthController(goqu, logger, config)
-	if err != nil {
-		return err
-	}
-	v1.Post("/login", authController.DoAuth)
-	return nil
-}
 
-func setupUserController(v1 fiber.Router, goqu *goqu.Database, logger *zap.Logger, middlewares middlewares.Middleware, events *events.Events) error {
-	userController, err := controller.NewUserController(goqu, logger, events)
-	if err != nil {
-		return err
-	}
-
-	userRouter := v1.Group("/users")
-	userRouter.Post("/", userController.CreateUser)
-	userRouter.Get(fmt.Sprintf("/:%s", constants.ParamUid), middlewares.Authenticated, userController.GetUser)
-	return nil
-}
 
 func healthCheckController(app *fiber.App, goqu *goqu.Database, logger *zap.Logger) error {
 	healthController, err := controller.NewHealthController(goqu, logger)
